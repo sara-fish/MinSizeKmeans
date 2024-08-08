050fd02a0c7e66ea2fcf801170edf52d71c51aa6
sara-fish <sara.mfish@gmail.com>
2024-08-07 21:46:46 -0400

    Pass random_obj (which is the self.random=Random(seed) of some generator class) through kmeans initialization, for determinism

--- minsize_kmeans/minmax_kmeans.py
+++ minsize_kmeans/minmax_kmeans.py
@@ -5,8 +5,10 @@ import pulp
 import random
 import argparse
 
+
 def l2_distance(point1, point2):
-    return sum([(float(i)-float(j))**2 for (i,j) in zip(point1, point2)])
+    return sum([(float(i) - float(j)) ** 2 for (i, j) in zip(point1, point2)])
+
 
 class subproblem(object):
     def __init__(self, centroids, data, min_size, max_size):
@@ -14,7 +16,7 @@ class subproblem(object):
         self.centroids = centroids
         self.data = data
         self.min_size = min_size
-        self.max_size= max_size
+        self.max_size = max_size
         self.n = len(data)
         self.k = len(centroids)
 
@@ -25,27 +27,33 @@ class subproblem(object):
             return l2_distance(self.data[assignment[0]], self.centroids[assignment[1]])
 
         clusters = list(range(self.k))
-        assignments = [(i, j)for i in range(self.n) for j in range(self.k)]
+        assignments = [(i, j) for i in range(self.n) for j in range(self.k)]
 
         # outflow variables for data nodes
-        self.y = pulp.LpVariable.dicts('data-to-cluster assignments',
-                                  assignments,
-                                  lowBound=0,
-                                  upBound=1,
-                                  cat=pulp.LpInteger)
+        self.y = pulp.LpVariable.dicts(
+            "data-to-cluster assignments",
+            assignments,
+            lowBound=0,
+            upBound=1,
+            cat=pulp.LpInteger,
+        )
 
         # outflow variables for cluster nodes
-        self.b = pulp.LpVariable.dicts('cluster outflows',
-                                  clusters,
-                                  lowBound=0,
-                                  upBound=self.n-self.min_size,
-                                  cat=pulp.LpContinuous)
+        self.b = pulp.LpVariable.dicts(
+            "cluster outflows",
+            clusters,
+            lowBound=0,
+            upBound=self.n - self.min_size,
+            cat=pulp.LpContinuous,
+        )
 
         # create the model
         self.model = pulp.LpProblem("Model for assignment subproblem", pulp.LpMinimize)
 
         # objective function
-        self.model += pulp.lpSum([distances(assignment) * self.y[assignment] for assignment in assignments])
+        self.model += pulp.lpSum(
+            [distances(assignment) * self.y[assignment] for assignment in assignments]
+        )
 
         # flow balance constraints for data nodes
         for i in range(self.n):
@@ -53,32 +61,38 @@ class subproblem(object):
 
         # flow balance constraints for cluster nodes
         for j in range(self.k):
-            self.model += pulp.lpSum(self.y[(i, j)] for i in range(self.n)) - self.min_size == self.b[j]
-            
+            self.model += (
+                pulp.lpSum(self.y[(i, j)] for i in range(self.n)) - self.min_size
+                == self.b[j]
+            )
+
         # capacity constraint on outflow of cluster nodes
         for j in range(self.k):
-            self.model += self.b[j] <= self.max_size - self.min_size 
+            self.model += self.b[j] <= self.max_size - self.min_size
 
         # flow balance constraint for the sink node
-        self.model += pulp.lpSum(self.b[j] for j in range(self.k)) == self.n - (self.k * self.min_size)
-
+        self.model += pulp.lpSum(self.b[j] for j in range(self.k)) == self.n - (
+            self.k * self.min_size
+        )
 
     def solve(self):
         self.status = self.model.solve()
 
         clusters = None
         if self.status == 1:
-            clusters= [-1 for i in range(self.n)]
+            clusters = [-1 for i in range(self.n)]
             for i in range(self.n):
                 for j in range(self.k):
                     if self.y[(i, j)].value() > 0:
                         clusters[i] = j
         return clusters
 
-def initialize_centers(dataset, k):
+
+def initialize_centers(dataset, k, random_obj):
     ids = list(range(len(dataset)))
-    random.shuffle(ids)
+    random_obj.shuffle(ids)
     return [dataset[id] for id in ids[:k]]
+
 
 def compute_centers(clusters, dataset):
     # canonical labeling of clusters
@@ -99,15 +113,16 @@ def compute_centers(clusters, dataset):
         counts[c] += 1
     for j in range(k):
         for i in range(dim):
-            centers[j][i] = centers[j][i]/float(counts[j])
+            centers[j][i] = centers[j][i] / float(counts[j])
     return clusters, centers
 
-def minsize_kmeans(dataset, k, min_size=0, max_size=None):
+
+def minsize_kmeans(dataset, k, min_size=0, max_size=None, random_obj=random):
     n = len(dataset)
     if max_size == None:
         max_size = n
 
-    centers = initialize_centers(dataset, k)
+    centers = initialize_centers(dataset, k, random_obj)
     clusters = [-1] * n
 
     converged = False
@@ -128,15 +143,17 @@ def minsize_kmeans(dataset, k, min_size=0, max_size=None):
 
     return clusters, centers
 
+
 def read_data(datafile):
     data = []
-    with open(datafile, 'r') as f:
+    with open(datafile, "r") as f:
         for line in f:
             line = line.strip()
-            if line != '':
+            if line != "":
                 d = [float(i) for i in line.split()]
                 data.append(d)
     return data
+
 
 def cluster_quality(cluster):
     if len(cluster) == 0:
@@ -148,6 +165,7 @@ def cluster_quality(cluster):
             quality += l2_distance(cluster[i], cluster[j])
     return quality / len(cluster)
 
+
 def compute_quality(data, cluster_indices):
     clusters = dict()
     for i, c in enumerate(cluster_indices):
@@ -157,27 +175,32 @@ def compute_quality(data, cluster_indices):
             clusters[c] = [data[i]]
     return sum(cluster_quality(c) for c in clusters.values())
 
-if __name__ == '__main__':
-    
+
+if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('datafile', help='file containing the coordinates of instances')
-    parser.add_argument('k', help='number of clusters', type=int)
-    parser.add_argument('min_size', help='minimum size of each cluster', type=int)  
-    parser.add_argument('max_size', help='maximum size of each cluster', type=int)      
-    parser.add_argument('-n', '--NUM_ITER', type=int,
-                        help='run the algorithm for NUM_ITER times and return the best clustering',
-                        default=1)
-    parser.add_argument('-o', '--OUTFILE', help='store the result in OUTFILE',
-                        default='')
-    args = parser.parse_args()    
-    
+    parser.add_argument("datafile", help="file containing the coordinates of instances")
+    parser.add_argument("k", help="number of clusters", type=int)
+    parser.add_argument("min_size", help="minimum size of each cluster", type=int)
+    parser.add_argument("max_size", help="maximum size of each cluster", type=int)
+    parser.add_argument(
+        "-n",
+        "--NUM_ITER",
+        type=int,
+        help="run the algorithm for NUM_ITER times and return the best clustering",
+        default=1,
+    )
+    parser.add_argument(
+        "-o", "--OUTFILE", help="store the result in OUTFILE", default=""
+    )
+    args = parser.parse_args()
+
     data = read_data(args.datafile)
 
     best = None
     best_clusters = None
     for i in range(args.NUM_ITER):
-        clusters, centers = minsize_kmeans(data, args.k, 
-                                           args.min_size, args.max_size)
+        clusters, centers = minsize_kmeans(data, args.k, args.min_size, args.max_size)
         if clusters:
             quality = compute_quality(data, clusters)
             if not best or (quality < best):
@@ -185,14 +208,12 @@ if __name__ == '__main__':
                 best_clusters = clusters
     if best:
         if args.OUTFILE:
-            with open(args.OUTFILE, 'w') as f:
-                print('\n'.join(str(i) for i in best_clusters), file=f)
+            with open(args.OUTFILE, "w") as f:
+                print("\n".join(str(i) for i in best_clusters), file=f)
         else:
-            print('cluster assignments:')
+            print("cluster assignments:")
             for i in range(len(best_clusters)):
-                print('%d: %d'%(i, best_clusters[i]))
-        print('sum of squared distances: %.4f'%(best))
+                print("%d: %d" % (i, best_clusters[i]))
+        print("sum of squared distances: %.4f" % (best))
     else:
-        print('no clustering found')
-
-
+        print("no clustering found")
